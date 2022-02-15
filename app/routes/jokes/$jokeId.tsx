5cc588dde65acc2cb0ab9b357363ddcfce6d8737
Saull Brandão <saull@outlook.com>
2022-02-15 09:42:53 -0300

    feat: display the selected joke content

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,30 @@
+import { LoaderFunction, useLoaderData } from 'remix'
+import type { Joke } from '@prisma/client'
+import { db } from '~/utils/db.server'
+import { Link } from 'react-router-dom'
+
+type LoaderData = { joke: Joke }
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  })
+
+  if (!joke) throw new Error('Joke not found')
+
+  const data: LoaderData = { joke }
+
+  return data
+}
+
 export default function JokeRoute() {
+  const { joke } = useLoaderData<LoaderData>()
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{joke.content}</p>
+      <Link to=".">{joke.name} Permalink</Link>
     </div>
   )
 }
